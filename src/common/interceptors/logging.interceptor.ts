06607d7453c1340a3133f3f0513a5afc70f3a8df
hewenke <hewenke00@163.com>
2026-06-09 13:50:15 +0800

    日志

---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,19 @@
+import { NestInterceptor, ExecutionContext, CallHandler, Injectable, Logger } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  private logger = new Logger('HTTP');
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const { method, url } = request;
+    const now = Date.now();
+    return next.handle().pipe(
+      tap(() => {
+        this.logger.log(`${method} ${url} ${Date.now() - now}ms`);
+      }),
+    );
+  }
+}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
+  private readonly logger = new Logger('Prisma');
+
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
 }
